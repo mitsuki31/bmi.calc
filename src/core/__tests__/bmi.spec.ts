d98644ecdb84ec481f7670781dddc4540a5c695d
Ryuu Mitsuki <dhefam31@proton.me>
2025-12-18 17:18:29 +0700

    test(core): Add test suites for `getCategory`

--- src/core/__tests__/bmi.spec.ts
+++ src/core/__tests__/bmi.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { calculateBMI, BMICalculationError } from '../bmi';
+import { BMICalculationError, calculateBMI, Category, getCategory } from '../bmi';
 
 describe('calculateBMI', function () {
   it('calculates BMI using meters', function () {
@@ -50,7 +50,39 @@ describe('calculateBMI', function () {
     }
 
     expect(bmiError).toBeInstanceOf(BMICalculationError);
-    expect(bmiError?.weightKg).toBe(-70);
-    expect(bmiError?.heightM).toBe(1.8);
+    expect(bmiError!.weightKg).toBe(-70);
+    expect(bmiError!.heightM).toBe(1.8);
+  });
+});
+
+describe('getCategory', function () {
+  it('returns the corresponding category from given BMI result', function () {
+    const bmiAndExpectedCategories = {
+      '16': Category.UNDERWEIGHT,
+      '18.4': Category.UNDERWEIGHT,
+      '18.5': Category.HEALTHY,
+      '24.8': Category.HEALTHY,
+      '24.9': Category.HEALTHY,
+      '25': Category.OVERWEIGHT,
+      '29.8': Category.OVERWEIGHT,
+      '29.9': Category.OVERWEIGHT,
+      '30': Category.OBESITY,
+      '100': Category.OBESITY,
+    };
+
+    for (const [bmi, category] of Object.entries(bmiAndExpectedCategories)) {
+      expect(getCategory(Number.parseFloat(bmi))).toStrictEqual(category);
+    }
+  });
+
+  it('does not throw but returns OBESITY when the input is Infinity or NaN', function () {
+    expect(getCategory(NaN)).toStrictEqual(Category.OBESITY);
+    expect(getCategory(Infinity)).toStrictEqual(Category.OBESITY);
+  });
+
+  it('does not throw but returns UNDERWEIGHT when the input is non-positive number', function () {
+    expect(getCategory(0)).toStrictEqual(Category.UNDERWEIGHT);
+    expect(getCategory(-1)).toStrictEqual(Category.UNDERWEIGHT);
+    expect(getCategory(-Infinity)).toStrictEqual(Category.UNDERWEIGHT);
   });
 });
