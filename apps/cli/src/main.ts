a9e08ba0c382b3159416e6aaee4f626877f2591d
Ryuu Mitsuki <dhefam31@proton.me>
2026-03-24 22:23:11 +0700

    feat(cli): Add main binary file for CLI app
    This file (`src/main.ts`) will be main binary file for `@bmi-calc/cli`
    and symlinked with name `bmi`

---
+++ apps/cli/src/main.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+import Yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import { APP_NAME, SCRIPT_NAME, VERSION } from './config/constants.js';
+import mainCommandBuilder from './commands/main.js';
+
+const maxWidth = process.stdout.columns || null;
+
+const yargs = Yargs(hideBin(process.argv))
+  .scriptName(SCRIPT_NAME)
+  .wrap(maxWidth);
+
+mainCommandBuilder(yargs)
+  .recommendCommands()
+
+  // -- Misc options
+  .version(false)
+  .option('version', {
+    alias: ['v'],
+    type: 'boolean',
+    description: 'Show version information',
+    global: true,
+    coerce: (value) => {
+      if (value) {
+        console.log(`${APP_NAME} v${VERSION}`);
+        process.exit(0);
+      }
+      return false;
+    }
+  })
+
+  .help()
+  .alias('?', 'help')
+
+  // -- Error handling
+  .strict()
+  .showHelpOnFail(false)
+  .fail((message, error) => {
+    if (message) console.error(message);
+    if (error) console.error(error);
+    console.error('See --help for usage information');
+    process.exit(process.exitCode || 1);
+  })
+  .parse();
