fed26e39a9cadbca441ca4cb4e33d7eaf107a028
Ryuu Mitsuki <dhefam31@proton.me>
2026-03-24 22:18:36 +0700

    feat(cli:types): Add global type definitions

---
+++ apps/cli/src/types/index.d.ts
@@ -0,0 +1,9 @@
+// Global type definitions for @bmi-calc/cli
+// Project: bmi-calc
+// Definitions by: Ryuu Mitsuki (https://github.com/mitsuki31)
+
+declare interface BMIHandlerArgv {
+  weight: number;
+  height: number;
+  unit: "cm" | "m";
+}
