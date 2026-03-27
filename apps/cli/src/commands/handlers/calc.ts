e8557bf7c7d3fadb26b5feaa4a1d5bccd47bab07
Ryuu Mitsuki <dhefam31@proton.me>
2026-03-28 00:17:25 +0700

    feat(cli:opts): Add several global options
    This options include:
      - `color`: Enable color output (default: true)
      - `no-color`: Disable color output
      - `json`: Output result in JSON format (alias: `-J`)
    
    `--json` option will automatically disable colors if the command is
    piped to a file or command.

--- apps/cli/src/commands/handlers/calc.ts
+++ apps/cli/src/commands/handlers/calc.ts
@@ -2,13 +2,17 @@
 
 import type { Argv } from 'yargs';
 import { calculateBMI, getCategory } from '@bmi-calc/core';
-import { printBMIResult } from '../../ui/outputs.js';
+import { printBMIResult, printBMIResultAsJSON } from '../../ui/outputs.js';
 
 export const calcHandler = (argv: Awaited<Argv['argv']>) => {
-  const { weight, height, unit } = argv as unknown as BMIHandlerArgv;
-
+  const { weight, height, unit, json } = argv as unknown as BMIHandlerArgv;
   const bmi = calculateBMI(weight, { value: height, unit });
   const category = getCategory(bmi);
 
+  if (json) {
+    // No worry about piping to file, the output will no have colors
+    printBMIResultAsJSON({ bmi, category }, process.stdout.isTTY);
+    return;
+  }
   printBMIResult({ bmi, category });
 };
--- apps/cli/src/commands/main.ts
+++ apps/cli/src/commands/main.ts
@@ -1,6 +1,7 @@
 import type { Argv, Options } from 'yargs';
 import bmiCommand from './bmi.js';
 import calcCommand from './calc.js';
+import globalOptions from './options/global.js';
 import miscOptions from './options/misc.js';
 
 function buildMiscOptions(yargs: Argv, opts: Record<string, Options>) {
@@ -9,15 +10,17 @@ function buildMiscOptions(yargs: Argv, opts: Record<string, Options>) {
 
 export default function builder(yargs: Argv): Argv {
   return buildMiscOptions(
-    // prettier-ignore
     yargs
       .usage('$0 --weight <N> --height <N> [--unit <cm|m>]')
+      // Set groups first
       .group([], 'Command Options:')
       .group([], 'Global Options:')
       .group([], 'Misc Options:')
 
       .command(bmiCommand)
       .command(calcCommand)
-    , miscOptions
+      .options(globalOptions)
+      .group(Object.keys(globalOptions), 'Global Options:'),
+    miscOptions,
   ).group([...Object.keys(miscOptions), 'help'], 'Misc Options:');
 }
