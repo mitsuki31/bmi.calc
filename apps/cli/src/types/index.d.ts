07a088f1d83340f66d5ff3b519b88badcd2849cd
Ryuu Mitsuki <dhefam31@proton.me>
2026-03-27 20:07:29 +0700

    feat(cli:types): Add BMI result typedefs

--- apps/cli/src/types/index.d.ts
+++ apps/cli/src/types/index.d.ts
@@ -3,7 +3,33 @@
 // Definitions by: Ryuu Mitsuki (https://github.com/mitsuki31)
 
 declare interface BMIHandlerArgv {
+  // --weight
+  w: number;
   weight: number;
+
+  // --height
+  h: number;
   height: number;
+
+  // --unit
+  u: 'cm' | 'm';
   unit: 'cm' | 'm';
+
+  // --json
+  J: boolean;
+  json: boolean;
+
+  // --no-color
+  'no-color': boolean;
+  noColor: boolean;
+}
+
+declare interface BMIResult {
+  bmi: number;
+  category: Category;
+}
+
+declare interface BMIResultJSON {
+  bmi: string; // Formatted to 2 decimal places
+  category: Capitalize<import('@bmi-calc/core').Category>;
 }
